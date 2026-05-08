301f03d2c3b2872a24e56c8848d37f1db43b826a
MX <10697207+xMasterX@users.noreply.github.com>
2026-05-08 22:13:39 +0300

    bring back this part

--- gps_reader.h
+++ gps_reader.h
@@ -2,7 +2,13 @@
 
 #include <furi_hal.h>
 
+// Define GPS_UART_CH per firmware origin
+#ifdef FW_ORIGIN_Momentum
+#include <momentum/momentum.h>
+#define GPS_UART_CH (momentum_settings.uart_nmea_channel)
+#else
 #define GPS_UART_CH (FuriHalSerialIdUsart)
+#endif
 
 #define GPS_RX_BUF_SIZE 512
 
